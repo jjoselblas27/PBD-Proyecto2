a7480c2f63344221a8f26d62493420234dbf3395
Alessandro del Piero Huayta Ecos <alessandro.huayta@utec.edu.pe>
2021-12-11 00:17:11 -0500

    cambios backend - agregando archivo sql

---
+++ backend/index.js
@@ -0,0 +1,503 @@
+const mysql = require('mysql');
+const express = require('express');
+
+
+var app = express();
+
+app.use(express.json());
+var cors = require('cors');
+
+app.use(cors());
+
+
+
+//comprobacion
+app.post('/login', function(req, res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = " SELECT id_user, username " +
+                " FROM usuarios " +
+                " WHERE username = ? " +
+                " AND password = MD5(?) ";
+  
+  var myValues = [ req.body.username, req.body.password ];
+  
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results[0]);
+
+    //pruebas:
+
+
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+});
+
+
+//registro de nuevos usuarios
+app.post('/register', function(req, res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = " INSERT INTO usuarios (username, password, correo, telefono, modified_date, created_date) " +
+                " VALUES (?, MD5(?), ?, ?, NOW(), NOW()) ";
+  
+  var myValues = [req.body.username, req.body.password, req.body.correo, req.body.telefono ];
+  
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+});
+
+
+//REGISTRO DE RECLAMOS 
+app.post('/reclamos', function(req, res){
+   // Step 0: Definir la conexion a la BD
+    var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = " INSERT INTO reclamos (nombre, apellido_p, apellido_m, direccion, tipo_documento, documento, telefono, email, edad, reclamo, modified_date, created_date) " +
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW()) ";
+  var myValues = [req.body.nombre, req.body.apellido_p, req.body.apellido_m, req.body.direccion, req.body.tipo_documento, req.body.documento, req.body.telefono, req.body.email, req.body.edad, req.body.reclamo];
+  
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+});
+
+
+//Update Datos del Usuario
+app.put('/usuario/:id_user', function(req, res){
+//Step 0: Definir la conexion a la BD
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'cliente',
+  password: '123456789',
+  database: 'restaurant'
+});
+
+// Step 1: Establecer la conexion
+connection.connect();
+// Step 2: Mandar el query
+  var myQuery = " UPDATE usuarios SET modified_date = NOW() ";
+  var myValues = [ ];
+  
+  if (req.body.username){
+    myQuery += " , username = ? ";
+    myValues.push(req.body.username);
+  }
+
+  if (req.body.password){
+    myQuery += " , password = MD5(?) ";
+    myValues.push(req.body.password);
+  }
+
+  if (req.body.correo){
+    myQuery += " , correo = ? ";
+    myValues.push(req.body.correo);
+  }
+
+  if (req.body.phone){
+    myQuery += " , telefono = ? ";
+    myValues.push(req.body.phone);
+  }
+
+  myQuery += " WHERE id_user = ? "
+  myValues.push(req.params.id_user);
+
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+});
+
+app.post('/comentarios_new', function(req,res){
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+  connection.connect();
+
+  var myQuery = "INSERT INTO comentarios(id_user, puntuacion, comentario, modified_date, created_date) "+
+                " VALUES(?, ?, ?, NOW(), NOW() )";
+  var myValues = [req.body.id_user, req.body.puntuacion, req.body.comentario];  
+
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+
+})
+
+
+// Mostrar Comentarios del Usuario:
+app.get('/comentarios', function(req, res){
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'cliente',
+        password: '123456789',
+        database: 'restaurant'
+    });
+
+    connection.connect();
+
+    var myQuery = " SELECT id_comentario, id_user, puntuacion, comentario, modified_date, created_date FROM comentarios WHERE 1 = 1 ";
+    var myValues = [];
+
+    connection.query(myQuery, myValues, function(error, results, fields){
+        // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+        if (error) throw error;
+        
+        // Step 3: Procesar el resultado de la BD
+        res.send(results);
+    
+        // Step 4: Cerrar la conexion
+        connection.end();
+      });
+    });
+
+
+//Update Comentario cuando esta logeado
+app.put('/comentarios/:id_comentario', function(req, res){
+  //Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+  
+  // Step 1: Establecer la conexion
+  connection.connect();
+ // Step 2: Mandar el query
+  var myQuery = " UPDATE comentarios SET modified_date = NOW() ";
+  var myValues = [ req.body.id_comentario, req.body.id_user];
+  
+  if (req.body.comentario){
+    myQuery += " , comentario = ? ";
+    myValues.push(req.body.comentario);
+  }
+
+
+  myQuery += " WHERE id_comentario = ? "
+  myValues.push(req.params.id_comentario);
+
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+});
+
+
+//Tabla de Pedidos
+app.post('/pedido_online', function(req, res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = " INSERT INTO usuarios (username, password, correo, telefono, modified_date, created_date) " +
+                " VALUES (?, MD5(?), ?, ?, NOW(), NOW()) ";
+  
+  var myValues = [ req.body.username, req.body.password, req.body.correo, req.body.telefono ];
+  
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+});
+
+
+//DELETE comentarios
+app.delete('/deleteComentarios', function(req,res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = "DELETE FROM comentarios "+
+                " WHERE id_comentario = ? AND id_user = ?";
+        
+  
+  var myValues = [ req.body.id_comentario, req.body.id_user];
+  
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+
+})
+
+
+//Registro de nueva reserva
+app.post('/reservaciones_new', function(req, res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+     host: 'localhost',
+     user: 'cliente',
+     password: '123456789',
+     database: 'restaurant'
+ });
+
+ // Step 1: Establecer la conexion
+ connection.connect();
+
+ // ;Step 2: Mandar el query
+ var myQuery =   " INSERT INTO reservaciones (nombre, " +
+                 " n_personas, tipo_mesa, hora_reservacion, fecha, correo," +
+                 " telefono, created_date, modified_date ) " +
+                 " VALUES (?,?,?,?,?,?,?, NOW(), NOW()); ";
+
+ var myValues = [req.body.nombre, req.body.n_personas, req.body.tipo_mesa, req.body.hora_reservacion, req.body.fecha, req.body.correo, req.body.telefono];
+
+ connection.query(myQuery, myValues, function(error, results, fields){
+     // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+     if (error) throw error;
+     
+     // Step 3: Procesar el resultado de la BD
+     res.send(results);
+
+     // Step 4: Cerrar la conexion
+     connection.end();
+ });
+});
+
+
+//DELETE reserva
+app.delete('/reserva/:id_user', function(req,res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = "DELETE FROM reservaciones "+
+                " WHERE id_reservacion = ? AND id_user = ?";
+        
+  var myValues = [req.body.id_reservacion, req.body.id_user];
+  
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+
+})
+
+
+//Mostrar las zonas de reparto
+app.get('/zonas_reparto', function(req,res){
+  // Step 0: Definir la conexion a la BD
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'cliente',
+    password: '123456789',
+    database: 'restaurant'
+  });
+
+  // Step 1: Establecer la conexion
+  connection.connect();
+
+  // Step 2: Mandar el query
+  var myQuery = " SELECT id_zona, distrito, disponibilidad FROM zonas_reparto WHERE 1 = 1; "; 
+  var myValues = [];      
+    
+  connection.query(myQuery, myValues, function(error, results, fields){
+    // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+    if (error) throw error;
+    
+    // Step 3: Procesar el resultado de la BD
+    res.send(results);
+
+    // Step 4: Cerrar la conexion
+    connection.end();
+  });
+
+})
+
+
+// Mostrar todos los platos de la carta con sus precios 
+app.get('/menu', function(req, res){
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'cliente',
+        password: '123456789',
+        database: 'restaurant'
+    });
+
+    connection.connect();
+
+    var myQuery = " SELECT plato_id, name_plato, tipo_plato, price, created_date, modified_date FROM menu WHERE 1 = 1 ";
+    var myValues = [];
+
+    connection.query(myQuery, myValues, function(error, results, fields){
+        // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+        if (error) throw error;
+        
+        // Step 3: Procesar el resultado de la BD
+        res.send(results);
+    
+        // Step 4: Cerrar la conexion
+        connection.end();
+      });
+    });
+
+
+// Actualizar datos de los platos 
+app.put('/menu/:plato_id', function(req, res){
+        // Step 0: Definir la conexion a la BD
+        var connection = mysql.createConnection({
+          host: 'localhost',
+          user: 'cliente',
+          password: '123456789',
+          database: 'restaurant'
+        });
+      
+        // Step 1: Establecer la conexion
+        connection.connect();
+      
+        // Step 2: Mandar el query
+        var myQuery = " UPDATE menu SET modified_date = NOW() ";
+        var myValues = [ ];
+        
+        if (req.body.name_plato){
+          myQuery += " , name_plato = ? ";
+          myValues.push(req.body.name_plato);
+        }
+      
+        if (req.body.tipo_plato){
+          myQuery += " , tipo_plato = ? ";
+          myValues.push(req.body.tipo_plato);
+        }
+      
+        if (req.body.price){
+          myQuery += " , price = ? ";
+          myValues.push(req.body.price);
+        }
+      
+      
+        myQuery += " WHERE plato_id = ? "
+        myValues.push(req.params.plato_id);
+      
+        connection.query(myQuery, myValues, function(error, results, fields){
+          // Ya tengo el resultado del query en `results`. Si hay algun error, llegará en `error`
+          if (error) throw error;
+          
+          // Step 3: Procesar el resultado de la BD
+          res.send(results);
+      
+          // Step 4: Cerrar la conexion
+          connection.end();
+        });
+      });
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("server 3000 abierto")
+})
---
+++ base_datos.sql
@@ -0,0 +1,120 @@
+CREATE DATABASE restaurant;
+USE restaurant;	
+
+CREATE TABLE usuarios(	
+    id_user int NOT NULL AUTO_INCREMENT,
+	username varchar(50) NOT NULL,
+	password varchar(255) NOT NULL,
+	correo varchar(255),
+	telefono int,
+    created_date DATETIME NOT NULL,
+    modified_date DATETIME NOT NULL,
+    PRIMARY KEY(id_user)
+);
+
+CREATE TABLE menu(
+	plato_id int NOT NULL auto_increment,
+	name_plato varchar(50) NOT null,
+	tipo_plato varchar(50) NOT null,
+	price float NOT null,
+	created_date datetime,
+	modified_date datetime,
+	PRIMARY kEY (plato_id)
+);
+
+CREATE TABLE reservaciones (
+	id_reservacion int NOT NULL AUTO_INCREMENT,
+	nombre varchar(50) NOT NULL,
+	n_personas int NOT NULL,
+	tipo_mesa ENUM('Ventana - Piso1','Ventana - Piso2','Dentro - Piso1','Dentro - Piso2') NOT NULL,
+	hora_reservacion time NOT NULL,
+	fecha datetime,	
+	correo varchar(255),
+	telefono int,
+	created_date datetime,
+	modified_date datetime,
+	PRIMARY KEY (id_reservacion)
+);
+
+CREATE TABLE pedidos_online(
+	id_pedido int NOT NULL AUTO_INCREMENT,
+	plato varchar(200) NOT NULL,
+	distrito enum('Callao', 'Comas', 'Carabayllo', 'San martin de Porres', 'Los Olivos') NOT NULL,
+	dirección varchar(255) NOT NULL,
+	id_user int NOT NULL,
+	PRIMARY KEY(id_pedido),
+	FOREIGN KEY (id_user) REFERENCES usuarios(id_user)
+);
+
+CREATE TABLE comentarios(
+	id_comentario int NOT NULL AUTO_INCREMENT,
+	id_user int NOT NULL,
+	puntuacion int NOT NULL,
+	comentario varchar(5000),
+    modified_date DATETIME NOT NULL,
+    created_date DATETIME NOT NULL,
+	PRIMARY KEY(id_comentario),
+	FOREIGN KEY (id_user) REFERENCES usuarios(id_user)
+);
+
+CREATE TABLE reclamos(
+	id_reclamo int NOT NULL AUTO_INCREMENT,
+	nombre varchar(60) NOT NULL,
+	apellido_p varchar(50)	NOT NULL,
+	apellido_m varchar(50) NOT NULL,
+	direccion varchar(150) NOT NULL,
+	tipo_documento enum('DNI','CARNET', 'PASAPORTE') NOT NULL,
+	documento int NOT NULL,
+	telefono int NOT NULL,
+	email varchar(255) NOT NULL,
+	edad int NOT NULL,
+	reclamo varchar(5000) NOT NULL,
+	created_date datetime,
+	modified_date datetime,
+	PRIMARY KEY (id_reclamo)
+);
+
+CREATE TABLE zonas_reparto(
+	id_zona int NOT NULL AUTO_INCREMENT,
+	distrito varchar(255) NOT NULL,
+	disponibilidad ENUM('Si','No') NOT NULL,
+	modified_date DATETIME,
+	created_date DATETIME,
+	PRIMARY KEY(id_zona)
+);
+
+
+--pruebas--
+INSERT INTO usuarios(id_user, username, password, correo, telefono, created_date, modified_date) 
+VALUES (1,'JUAN', MD5('12345'), NULL, NULL, NOW(), NOW());
+INSERT INTO usuarios(id_user, username, password, correo, telefono, created_date, modified_date) 
+VALUES (2,'JOSE', MD5('54321'), NULL, NULL, NOW(), NOW());
+
+INSERT INTO comentarios(id_comentario, id_user, puntuacion, comentario, modified_date, created_date)
+VALUES (1,1,5,"maso el restaurante :V", NOW(), NOW());
+INSERT INTO comentarios(id_comentario, id_user, puntuacion, comentario, modified_date, created_date)
+VALUES (2,2,3,"a nada :|", NOW(), NOW());
+
+insert into menu values(1, 'California Roll', 'Maki', '25', '2019-09-14', '2020-08-15');
+insert into menu values(2, 'Acevichado', 'Maki', '28' , '2020-07-14', '2020-09-15');
+insert into menu values(3, 'Causa Roll ', 'Maki', '25', '2020-09-14', '2021-08-15');
+insert into menu values(4, 'Lomo Saltado Maki ', 'Maki', '20', '2020-09-14', '2021-08-15');
+INSERT INTO menu(plato_id, name_plato, tipo_plato, price, created_date, modified_date)
+VALUES(5, "Ramen", "Sopa", 20.00, NOW(), NOW());
+INSERT INTO menu(plato_id, name_plato, tipo_plato, price, created_date, modified_date)
+VALUES(6, "Yakitori","brocheta", 40.00, NOW(), NOW());
+
+INSERT INTO zonas_reparto(id_zona, distrito, disponibilidad, modified_Date, created_Date)
+VALUES (1,'Callao','Si',NOW(),NOW());
+INSERT INTO zonas_reparto(id_zona, distrito, disponibilidad, modified_Date, created_Date)
+VALUES (2,'Comas','Si',NOW(),NOW());
+INSERT INTO zonas_reparto(id_zona, distrito, disponibilidad, modified_Date, created_Date)
+VALUES (3, 'Carabayllo','Si',NOW(),NOW());
+INSERT INTO zonas_reparto(id_zona, distrito, disponibilidad, modified_Date, created_Date)
+VALUES (4, 'San Martin de Porres','Si',NOW(),NOW());
+INSERT INTO zonas_reparto(id_zona, distrito, disponibilidad, modified_Date, created_Date)
+VALUES (5, 'Los Olivos','No',NOW(),NOW());
+
+
+INSERT INTO reclamos (id_reclamo, nombre, apellido_p, apellido_m, direccion, tipo_documento, documento, telefono, email, edad, reclamo, modified_date, created_date) 
+VALUES (1,'ale','a','b','av.are','DNI', 73011282, 972043522,'alessandro@gmail.com',19,'pesimo restaurante :/',NOW(), NOW());
